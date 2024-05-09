08574b63b019278f10393bf3e3dd8e778f1946e5
lisuhang <aiyu5200414.@163.com>
2024-05-09 18:35:46 +0800

    idea commit third

--- src/main/java/demo01.java
+++ src/main/java/demo01.java
@@ -2,5 +2,6 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("工程");
         System.out.println("工程2");
+        System.out.println("工程3");
     }
 }
